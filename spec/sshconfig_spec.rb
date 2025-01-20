a8b1c303d13615aea1ae4dfb97005ab41698d856
ykominami <ykominami@gmail.com>
2025-01-20 19:06:26 +0900

    initialize commit

---
+++ spec/sshconfig_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Sshconfig do
+  it "has a version number" do
+    expect(Sshconfig::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
